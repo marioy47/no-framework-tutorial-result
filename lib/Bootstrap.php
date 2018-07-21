40f49a0a191f5a17b5b244d8dd3ff1d7f8481ecb
Mario Yepes <marioy47@gmail.com>
2018-07-21 18:42:29 -0500

    Agregando Whoops a Bootstrap

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -1,3 +1,22 @@
 <?php declare(strict_types = 1);
 
-echo 'Hola mundo'.time();
+namespace NoFramework;
+require __DIR__ .'/../vendor/autoload.php';
+define('ENVIRONMENT', 'development');
+error_reporting(E_ALL);
+
+/**
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run;
+if (ENVIRONMENT != 'production') {
+    $whoops->pushHandler( new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler( function($e) {
+        echo 'Todo: Enviarle email al desarrollador';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception("Esto es un error");
